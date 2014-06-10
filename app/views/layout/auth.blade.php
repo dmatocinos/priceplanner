453ce149a24684b8d38afec314a308ede099d39c
Dixie Philamerah J. Atay <dixie.atay@gmail.com>
2014-06-10 21:11:08 +0800

    changed bizval logos

--- app/views/layout/auth.blade.php
+++ app/views/layout/auth.blade.php
@@ -51,7 +51,7 @@
 	<div class="row">
 		<div class="col-md-3 col-sm-3" style="margin-left: 20px; margin-bottom: 0">
 			<div style="width: 290px;">
-				<a href="http://bizvaluationpro.practicepro.co.uk" class="thumbnail" title=" Create a professional business valuation in just 15 minutes"><img src="{{ asset('images/app-logos/bizvaluation_logo.png') }}" style="width: 80%; padding: 10px 0;"/></a>
+				<a href="http://bizvaluationpro.practicepro.co.uk" class="thumbnail" title=" Create a professional business valuation in just 15 minutes"><img src="{{ asset('images/app-logos/bizvaluation_logo.png') }}" style="width: 100%; padding: 10px 0;"/></a>
 			</div>
 			<div style="width: 290px;">
 				<a href="http://incorporationplannerpro.practicepro.co.uk" class="thumbnail" title="Show your clients the potential tax savings of incorporating their business"><img src="{{ asset('images/app-logos/incorporationplannerpro_logo.png') }}" style="width: 68%;"/></a>
