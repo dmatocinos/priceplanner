853b79e339a9155643c481184dc90d1f92683b6f
Dixie Philamerah Atay <dixie.atay@gmail.com>
2015-04-17 10:49:46 +0000

    base.blade.php edited online with Bitbucket

--- app/views/layout/base.blade.php
+++ app/views/layout/base.blade.php
@@ -5,6 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Dixie Philamerah J. Atay">
 
     <title>@yield('page_title') | Price Planner Pro</title>
     {{ Asset::container('header')->styles() }}
